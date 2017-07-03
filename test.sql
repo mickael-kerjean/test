456cb5f971bf90ad921deef287ff35e1a8a68a3e
Nuage <https://nuage.kerjean.me>
2017-07-03 11:43:01 +1000

    write(test.sql): /test.sql

--- test.sql
+++ test.sql
@@ -1,4 +1,4 @@
-select first_name, last_name, order_date, order_amount
+SELECT first_name, last_name, order_date, order_amount
 from customers c
 inner join orders o
 on c.customer_id = o.customer_id
